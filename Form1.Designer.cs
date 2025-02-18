28e453504144da686eee698df16dfe0bcbf403ff
khalyanakartika <khalyanakartika@gmail.com>
2025-02-18 21:36:32 +0700

    Ignore .vs folder

---
+++ Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿namespace selasa
+{
+    partial class kalkulator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            input_angka = new TextBox();
+            button_c = new Button();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button0 = new Button();
+            button_koma = new Button();
+            button_kali = new Button();
+            button_bagi = new Button();
+            button_tambah = new Button();
+            button_kurang = new Button();
+            button_samadengan = new Button();
+            SuspendLayout();
+            // 
+            // input_angka
+            // 
+            input_angka.BackColor = SystemColors.InactiveCaption;
+            input_angka.Cursor = Cursors.IBeam;
+            input_angka.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            input_angka.Location = new Point(16, 27);
+            input_angka.Multiline = true;
+            input_angka.Name = "input_angka";
+            input_angka.Size = new Size(242, 69);
+            input_angka.TabIndex = 2;
+            input_angka.TextAlign = HorizontalAlignment.Right;
+            input_angka.TextChanged += input_angka_TextChanged;
+            // 
+            // button_c
+            // 
+            button_c.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_c.ForeColor = SystemColors.ControlText;
+            button_c.Location = new Point(16, 116);
+            button_c.Name = "button_c";
+            button_c.Size = new Size(118, 56);
+            button_c.TabIndex = 3;
+            button_c.Text = "C";
+            button_c.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(16, 178);
+            button1.Name = "button1";
+            button1.Size = new Size(56, 56);
+            button1.TabIndex = 4;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(78, 178);
+            button2.Name = "button2";
+            button2.Size = new Size(56, 56);
+            button2.TabIndex = 5;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(140, 178);
+            button3.Name = "button3";
+            button3.Size = new Size(56, 56);
+            button3.TabIndex = 6;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button4.Location = new Point(16, 240);
+            button4.Name = "button4";
+            button4.Size = new Size(56, 56);
+            button4.TabIndex = 7;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button5.Location = new Point(78, 240);
+            button5.Name = "button5";
+            button5.Size = new Size(56, 56);
+            button5.TabIndex = 8;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button6.Location = new Point(140, 240);
+            button6.Name = "button6";
+            button6.Size = new Size(56, 56);
+            button6.TabIndex = 9;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button7.Location = new Point(16, 302);
+            button7.Name = "button7";
+            button7.Size = new Size(56, 56);
+            button7.TabIndex = 10;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.Location = new Point(78, 302);
+            button8.Name = "button8";
+            button8.Size = new Size(56, 56);
+            button8.TabIndex = 11;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button9.Location = new Point(140, 302);
+            button9.Name = "button9";
+            button9.Size = new Size(56, 56);
+            button9.TabIndex = 12;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            button0.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button0.Location = new Point(16, 364);
+            button0.Name = "button0";
+            button0.Size = new Size(118, 56);
+            button0.TabIndex = 13;
+            button0.Text = "0";
+            button0.UseVisualStyleBackColor = true;
+            // 
+            // button_koma
+            // 
+            button_koma.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_koma.Location = new Point(140, 364);
+            button_koma.Name = "button_koma";
+            button_koma.Size = new Size(56, 56);
+            button_koma.TabIndex = 14;
+            button_koma.Text = ".";
+            button_koma.UseVisualStyleBackColor = true;
+            // 
+            // button_kali
+            // 
+            button_kali.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_kali.ImageAlign = ContentAlignment.BottomCenter;
+            button_kali.Location = new Point(140, 116);
+            button_kali.Name = "button_kali";
+            button_kali.Size = new Size(56, 56);
+            button_kali.TabIndex = 15;
+            button_kali.Text = "*";
+            button_kali.UseVisualStyleBackColor = true;
+            // 
+            // button_bagi
+            // 
+            button_bagi.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_bagi.Location = new Point(202, 116);
+            button_bagi.Name = "button_bagi";
+            button_bagi.Size = new Size(56, 56);
+            button_bagi.TabIndex = 16;
+            button_bagi.Text = "/";
+            button_bagi.UseVisualStyleBackColor = true;
+            // 
+            // button_tambah
+            // 
+            button_tambah.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_tambah.Location = new Point(202, 178);
+            button_tambah.Name = "button_tambah";
+            button_tambah.Size = new Size(56, 56);
+            button_tambah.TabIndex = 17;
+            button_tambah.Text = "+";
+            button_tambah.UseVisualStyleBackColor = true;
+            // 
+            // button_kurang
+            // 
+            button_kurang.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_kurang.Location = new Point(202, 240);
+            button_kurang.Name = "button_kurang";
+            button_kurang.Size = new Size(56, 56);
+            button_kurang.TabIndex = 18;
+            button_kurang.Text = "-";
+            button_kurang.UseVisualStyleBackColor = true;
+            // 
+            // button_samadengan
+            // 
+            button_samadengan.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button_samadengan.Location = new Point(202, 302);
+            button_samadengan.Name = "button_samadengan";
+            button_samadengan.Size = new Size(56, 118);
+            button_samadengan.TabIndex = 19;
+            button_samadengan.Text = "=";
+            button_samadengan.UseVisualStyleBackColor = true;
+            // 
+            // kalkulator
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(276, 438);
+            Controls.Add(button_samadengan);
+            Controls.Add(button_kurang);
+            Controls.Add(button_tambah);
+            Controls.Add(button_bagi);
+            Controls.Add(button_kali);
+            Controls.Add(button_koma);
+            Controls.Add(button0);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(button_c);
+            Controls.Add(input_angka);
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Name = "kalkulator";
+            Text = "kalkulator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox input_angka;
+        private Button button_c;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button0;
+        private Button button_koma;
+        private Button button_kali;
+        private Button button_bagi;
+        private Button button_tambah;
+        private Button button_kurang;
+        private Button button_samadengan;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace selasa
+{
+    public partial class kalkulator : Form
+    {
+        public kalkulator()
+        {
+            InitializeComponent();
+        }
+
+        private void Kalkulator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_angka_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
